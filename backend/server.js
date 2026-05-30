aecf5bc5134b48beccd8f775729eeccf60153bdb
Abhijit694 <nayakabhijit543@gmail.com>
2026-05-30 20:49:57 +0530

    feat: setup mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+import dotenv from "dotenv"
+dotenv.config()
 import express from "express"
+import connectDB from "./config/db.js"
+
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 3000
 
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
+    connectDB()
 })
