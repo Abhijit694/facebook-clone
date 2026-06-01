c480ccb8031ed2338fd13b2a5b48c2c7fc447ca8
Abhijit694 <nayakabhijit543@gmail.com>
2026-06-01 10:52:50 +0530

    fix: take code out of wrong if block

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,28 +9,28 @@ export const registerUser = async (req,res) => {
                 success: false,
                 message: "Something missing"
             })
-            const existingUser = await User.findOne({ email })
-            if(existingUser){
-                return res.status(400).json({
-                    success: false,
-                    message: "Email id taken try another email id"
-                })
-            }
-            const hashedPassword = await bcrypt.hash(10,password)
-            const newUser = new User({
-                firstname,
-                lastname,
-                email,
-                password: hashedPassword,
-                gender,
-                dateOfBirth
-            })
-            await newUser.save()
-            return res.status(201).json({
-                success: true,
-                message: "Account created successfully"
+        }
+        const existingUser = await User.findOne({ email })
+        if(existingUser){
+            return res.status(400).json({
+                success: false,
+                message: "Email id taken try another email id"
             })
         }
+        const hashedPassword = await bcrypt.hash(10,password)
+        const newUser = new User({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword,
+            gender,
+            dateOfBirth
+        })
+        await newUser.save()
+        return res.status(201).json({
+            success: true,
+            message: "Account created successfully"
+        })
     } catch (error) {
         console.log(error);
         return res.status(500).json({
