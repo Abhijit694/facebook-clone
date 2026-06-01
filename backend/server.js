ebee5261c3d670005d620b2a95151199e3a1a629
Abhijit694 <nayakabhijit543@gmail.com>
2026-06-01 10:25:13 +0530

    feat: add authRouter to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
 import connectDB from "./config/db.js"
-
+import authRouter from "./routes/auth.route.js"
 
 const app = express()
 const port = process.env.PORT || 3000
+
+
+// api
+app.use("/api/v1/auth",authRouter)
 
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
