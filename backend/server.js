2ce45f4a5f0fe18e89c1a00252fdc24cadb9df0a
Abhijit694 <nayakabhijit543@gmail.com>
2026-06-05 23:17:06 +0530

    feat : cors middleware setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ dotenv.config()
 import express from "express"
 import connectDB from "./config/db.js"
 import authRouter from "./routes/auth.route.js"
+import cors from "cors"
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,6 +12,11 @@ const port = process.env.PORT || 3000
 // Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
 
 
 // api
