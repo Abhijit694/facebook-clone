189dc98f77d8683fe7c3ecdc413ba2083bc3cfdd
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-16 21:13:18 +0530

    feat : wrap App component with ThemeProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import ThemeProvider from './components/ThemeProvider'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={ store }>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Provider>
   
 )
