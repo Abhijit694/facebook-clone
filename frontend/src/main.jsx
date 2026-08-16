435561946ec4e0f5d6a340d1a6d9111fe8f9aa0b
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-16 23:39:22 +0530

    feat : add and implement Toast from shadcn

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import ThemeProvider from './components/ThemeProvider'
+import { Toaster } from "@/components/ui/toast"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={ store }>
     <ThemeProvider>
       <App />
+      <Toaster/>
     </ThemeProvider>
   </Provider>
   
