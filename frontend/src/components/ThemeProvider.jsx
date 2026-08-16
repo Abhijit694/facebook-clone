338693d959693e9376a5d3a4a7a56e167dac4cf7
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-16 21:12:25 +0530

    feat : implement ThemeProvider component

---
+++ frontend/src/components/ThemeProvider.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux"
+
+
+const ThemeProvider = ({children}) => {
+    const { theme } = useSelector(store => store.theme) 
+
+  return (
+    <div className={theme}>
+        <div className="bg-gray-200 text-gray-800 dark:text-gray-200 dark:bg-[#1b1b1c]">
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default ThemeProvider
