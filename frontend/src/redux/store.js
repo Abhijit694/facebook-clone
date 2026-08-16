712e0c7ed1a578ec1ad0f8a8f50dac96f750cbf1
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-16 23:41:07 +0530

    feat : implemented auth slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from './themeSlice'
+import authReducer from './authSlice'
 
 const store = configureStore({
   reducer: {
-    theme: themeReducer
+    theme: themeReducer,
+    auth: authReducer
   }
 })
 
