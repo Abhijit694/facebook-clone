02b2cd13afc592777bd15f1ee89f935220250929
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-18 23:08:22 +0530

    feat : create path for Layout, Home and Friends component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,24 @@ import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 import ProtectedRoute from './components/ProtectedRoute'
+import Layout from './components/Layout'
+import Friends from './pages/Friends'
 
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <><ProtectedRoute><Home/></ProtectedRoute></>
+    element: <ProtectedRoute><Layout/></ProtectedRoute>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/friends',
+        element:<Friends/>
+      }
+    ]
   },
   {
     path: '/login',
