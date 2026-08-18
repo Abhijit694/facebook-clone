429524dfb174db7915107ac445a159fc07f9211d
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-18 23:13:27 +0530

    feat : added MidHome and SponsorsSidebar component to Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
+import MidHome from '@/components/MidHome'
+import SponsorsSidebar from '@/components/SponsorsSidebar'
 
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <div className='flex gap-7 bg-[#f2f4f7] dark:bg-[#1b1b1c]'>
+        <MidHome/>
+        <SponsorsSidebar/>
+      </div>
     </div>
   )
 }
