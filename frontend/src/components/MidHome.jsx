fa84d92af21f2ee39bed32306d70a7e282460c1f
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-20 19:39:41 +0530

    feat : apply style to midhome and add createpost component to it

--- frontend/src/components/MidHome.jsx
+++ frontend/src/components/MidHome.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CreatePost from './CreatePost'
 
 const MidHome = () => {
   return (
-    <div>MidHome</div>
+    <div className='pt-16 md:pl-105 bg-[#f2f4f7] dark:bg-[#1b1b1c] flex-1 justify-center px-3 md:px-0'>
+      <CreatePost/>
+    </div>
   )
 }
 
