2b9fc75eac910383e0a78312c9dae7c7390ef7fd
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-21 22:11:52 +0530

    feat : postcard  component added to midhome component

--- frontend/src/components/MidHome.jsx
+++ frontend/src/components/MidHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CreatePost from './CreatePost'
+import PostCard from './PostCard'
 
 const MidHome = () => {
   return (
     <div className='pt-16 md:pl-105 bg-[#f2f4f7] dark:bg-[#1b1b1c] flex-1 justify-center px-3 md:px-0'>
       <CreatePost/>
+      <PostCard/>
     </div>
   )
 }
