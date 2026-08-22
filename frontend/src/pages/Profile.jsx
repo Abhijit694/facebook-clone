b1ef08479ee2069471ee1f7f65f095c21423dd60
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-22 22:28:10 +0530

    feat : profile page created

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='min-h-screen '>
+        <div className='relative w-full'>
+            {/* Blurred background */}
+            <div className='absolute inset-0 bg-center bg-cover filter'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
