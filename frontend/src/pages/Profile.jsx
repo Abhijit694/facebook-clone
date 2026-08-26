435f4ee269d53324d42dd4a6b15ceeffb0a2d5f7
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-26 23:36:17 +0530

    feat: implemented user profile picture in profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,38 +1,95 @@
-import defaultCoverImage from '../assets/default-cover-image.png'
+import defaultCoverImage from "../assets/default-cover-image.png";
 import { IoCamera } from "react-icons/io5";
-import userLogo from "../assets/fb-user-profile.jpg"
+import userLogo from "../assets/fb-user-profile.jpg";
+import { LuSquareUser } from "react-icons/lu";
+import { LuCamera } from "react-icons/lu";
+import { PiBookOpenBold } from "react-icons/pi";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
 
 const Profile = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className='min-h-screen '>
-        <div className='relative w-full'>
-            {/* Blurred background */}
-            <div
-              className='absolute inset-0 bg-center bg-cover filter'
-              style={{backgroundImage: `url(${defaultCoverImage})`}}
-            ></div>
-            {/* Overlay to darken he blur */}
-            <div className='absolute inset-0 bg-black/40 backbrop-blur-lg bg-linear-to-b from-transparent dark:to-[#262829] to-white'></div>
+    <div className="min-h-screen ">
+      <div className="relative w-full">
+        {/* Blurred background */}
+        <div
+          className="absolute inset-0 bg-center bg-cover filter"
+          style={{ backgroundImage: `url(${defaultCoverImage})` }}
+        ></div>
+        {/* Overlay to darken he blur */}
+        <div className="absolute inset-0 bg-black/40 backbrop-blur-lg bg-linear-to-b from-transparent dark:to-[#262829] to-white"></div>
 
-            {/* Actual cover image */}
-            <div className='relative flex justify-center items-center'>
-              <img src={defaultCoverImage} alt="cover" className='rounded-b-md w-full lg:w-240 h-60 md:h-100 object-cover' />
-              <div className='absolute right-5 md:right-55 bottom-3 md:bottom-5 flex items-center gap-2 bg-white hover:bg-gray-100 cursor-pointer text-black py-2 px-3 rounded-md'>
-                <IoCamera className='text-lg'/>
-                <span className='text-[15px] font-medium'>Add Cover Photo</span>
-              </div>
-            </div>
-        </div>
-
-        <div className='bg-white dark:bg-[#262829] z-40 py-4'>
-          <div className='max-w-240 mx-auto px-5 md:px-10 flex flex-col md:flex-row gap-2 md:gap-0 md:justify-between'>
-            <div className='flex flex-col md:flex-row md:gap-5 md:items-center relative'>
-              <img src={userLogo} className='size-44 cursor-pointer hover:invert-25 rounded-full border-4 border-white dark:border-[#262829] object-cover z-30 -mt-14'/>
-            </div>
+        {/* Actual cover image */}
+        <div className="relative flex justify-center items-center">
+          <img
+            src={defaultCoverImage}
+            alt="cover"
+            className="rounded-b-md w-full lg:w-240 h-60 md:h-100 object-cover"
+          />
+          <div className="absolute right-5 md:right-55 bottom-3 md:bottom-5 flex items-center gap-2 bg-white hover:bg-gray-100 cursor-pointer text-black py-2 px-3 rounded-md">
+            <IoCamera className="text-lg" />
+            <span className="text-[15px] font-medium">Add Cover Photo</span>
           </div>
         </div>
-    </div>
-  )
-}
+      </div>
 
-export default Profile
+      <div className="bg-white dark:bg-[#262829] z-40 py-4">
+        <div className="max-w-240 mx-auto px-5 md:px-10 flex flex-col md:flex-row gap-2 md:gap-0 md:justify-between">
+          <div className="flex flex-col md:flex-row md:gap-5 md:items-center relative">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <img
+                  src={userLogo}
+                  className="size-44 cursor-pointer hover:invert-25 rounded-full border-4 border-white dark:border-[#262829] object-cover z-30"
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-60">
+                <DropdownMenuItem className="flex items-center gap-2 cursor-pointer">
+                  <PiBookOpenBold />
+                  <span className="text-base font-medium">View story</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="flex items-center gap-2 cursor-pointer"
+                  onClick={() => setOpen(true)}
+                >
+                  <LuSquareUser />
+                  <span className="text-base font-medium">
+                    See profile picture
+                  </span>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="flex items-center gap-2 cursor-pointer">
+                  <LuCamera />
+                  <span className="text-base font-medium">
+                    Choose profile picture
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            
+            <Dialog>
+              <DialogTrigger>Open</DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
