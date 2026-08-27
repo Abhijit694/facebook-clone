fddb75f58cf75942d7f6908aaab123f53eca70d6
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-27 22:47:50 +0530

    feat : isAuthenticated middleware created

---
+++ backend/middlewares/isAuthenticated.js
@@ -0,0 +1,22 @@
+export const isAuthenticated = async (req,res,next) => {
+    try {
+        const token = req.cookies.token
+        if(!token){
+            return res.status(401).json({
+                message: "User not authenticated",
+                success: false
+            })
+        }
+        const decode = JsonWebTokenError.verify(token,process.env.JWT_TOKEN_SECRET)
+        if(!decode){
+            return res.status(401).json({
+                message: "Invalid token",
+                success: false
+            })
+        }
+        req.id = decode.userId
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+}
