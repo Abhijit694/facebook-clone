8db0026716bf83aed29fcc93f39891d8404cc317
Abhijit694 <nayakabhijit543@gmail.com>
2026-08-27 19:06:38 +0530

    feat : post model created

---
+++ backend/models/post.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+
+const postSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    content: {
+        type: String,
+    },
+    image: {
+        type: String
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Commemt"
+    }],
+    share: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
+},{ timestamps: true})
+
+const Post = mongoose.model("Post",postSchema)
+export default Post;
