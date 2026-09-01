6fcbebdf72bc5a4a87b0ca15ee09245ed421909c
Abhijit694 <nayakabhijit543@gmail.com>
2026-09-01 23:36:00 +0530

    feat : route created for updae profile picture and update cover photo

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,32 @@
-import express from "express"
-import { getProfile, loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js"
+import express from "express";
+import {
+  getProfile,
+  loginUser,
+  logoutUser,
+  registerUser,
+  updateCoverPhoto,
+  updateProfilePhoto,
+} from "../controllers/auth.controller.js";
+import { isAuthenticated } from "../middlewares/isAuthenticated.js";
+import { upload } from "../middlewares/multer.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register',registerUser)
-router.post('/login',loginUser)
-router.get('/logout',logoutUser)
-router.get('/profile/:id',getProfile)
-
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/profile/:id", getProfile);
+router.put(
+  "/update/profile-picture",
+  isAuthenticated,
+  upload.single("file"),
+  updateProfilePhoto,
+);
+router.put(
+  "/update/cover-photo",
+  isAuthenticated,
+  upload.single("file"),
+  updateCoverPhoto,
+);
 
 export default router;
