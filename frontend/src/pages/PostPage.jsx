5c38af5d4b59c30758c12cb054c6431a29f59d80
Abhijit694 <nayakabhijit543@gmail.com>
2026-09-04 23:28:49 +0530

    feat : postpage from profile page created

---
+++ frontend/src/pages/PostPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <div className='flex flex-col md:flex-row max-w-240 mx-auto gap-2 md:gap-5 mt-2 md:mt-5 px-2 md:px-10'>
+        <div>
+            {/* intro */}
+            
+        </div>
+    </div>
+  )
+}
+
+export default PostPage
