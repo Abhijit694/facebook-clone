eebd23fc98227ac8d8d85fdb49ca7315991d6798
Abhijit694 <nayakabhijit543@gmail.com>
2026-09-05 23:34:47 +0530

    feat : intro component added to post page

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,3 +1,4 @@
+import Intro from '@/components/Intro'
 import React from 'react'
 
 const PostPage = () => {
@@ -5,7 +6,7 @@ const PostPage = () => {
     <div className='flex flex-col md:flex-row max-w-240 mx-auto gap-2 md:gap-5 mt-2 md:mt-5 px-2 md:px-10'>
         <div>
             {/* intro */}
-            
+            <Intro/>
         </div>
     </div>
   )
