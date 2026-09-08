a0ad29467a9759388a5520d3b8ee939d9c51ace1
Abhijit694 <nayakabhijit543@gmail.com>
2026-09-08 23:25:31 +0530

    feat : personal details added and add a button for edit personal details

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -6,40 +6,53 @@ import { MdHomeWork } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaPhone } from "react-icons/fa6";
 import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const Intro = () => {
+
+  const { userProfile } = useSelector(store => store.auth)
+
+  const [introData, setIntroData] = useState({
+    bioText: userProfile?.bio?.bioText,
+    liveIn: userProfile?.bio?.liveIn,
+    relationship: userProfile?.bio?.relationship,
+    workplace: userProfile?.bio?.workplace,
+    education: userProfile?.bio?.education,
+    phone: userProfile?.bio?.phone,
+    hometown: userProfile?.bio?.hometown
+  })
 
     const arr1 = [
         {
             icon: <FaBriefcase/>,
-            text: 'Works at Web developer'
-        },
-        {
-            icon: <FaGraduationCap className="text-[18px]" />,
-            text: 'Studied at Lokanath Mohavidyalaya'
-        },
-        {
-            icon: <MdHomeWork className="text-[18px]" />,
-            text: 'From Kendrapara,Odisha'
+            text: `Works at ${introData.workplace}`
         },
         {
             icon: <FaLocationDot className="text-[18px]" />,
-            text: 'Lives in Kendrapara'
+            text: `Lives in ${introData.liveIn}`
+        },
+        {
+            icon: <FaGraduationCap className="text-[18px]" />,
+            text: `Studied at ${introData.education}`
+        },
+        {
+            icon: <MdHomeWork className="text-[18px]" />,
+            text: `From ${introData.hometown}`
         },
         {
             icon: <FaPhone/>,
-            text: '+91 3947343794'
+            text: `${introData.phone}`
         },
         {
             icon: <FaHeart/>,
-            text: 'Single'
+            text: `${introData.relationship}`
         },
     ]
 
   return (
-    <div className="bg-white dark:bg-[#262829] flex-1 rounded-lg p-5 h-max" >
-      <h1 className="text-xl font-bold mb-2 text-gray-700 dark:text-gray-200" >Intro</h1>
-      <p className="mb-5" >Hey I am a mernstack developer</p>
+    <div className="bg-white dark:bg-[#262829] flex-1 rounded-lg p-5 h-max shadow-md" >
+      <h1 className="text-xl font-bold mb-2 text-gray-700 dark:text-gray-200" >Personal details</h1>
       {
         arr1.map((item,index) => {
           return (
@@ -50,6 +63,7 @@ const Intro = () => {
           )
         })
       }
+      <button className="w-full bg-[#e1e4e8] hover:bg-[#c8cdd2] dark:bg-[#3a3c3d] text-gray-800 dark:text-gray-200 cursor-pointer py-1 rounded-md font-medium" >Edit details</button>
     </div>
   )
 }
