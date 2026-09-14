f864366d4112b668e8c5a81fba2a51ac36af7c5c
Abhijit694 <nayakabhijit543@gmail.com>
2026-09-14 23:30:16 +0530

    feat : edit details dialog fully implemented

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -15,6 +15,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogFooter
 } from "@/components/ui/dialog"
 
 const Intro = () => {
@@ -58,6 +59,18 @@ const Intro = () => {
         },
     ]
 
+    const editDetailsHandler = (e) => {
+      const { name,value } = e.target
+      setIntroData((prev) => ({
+        ...prev,
+        [name]:value
+      }))
+    }
+
+    const editRelationshipHandler = (value) => {
+      setIntroData({...introData, relationship: value})
+    }
+
   return (
     <div className="bg-white dark:bg-[#262829] flex-1 rounded-lg p-5 h-max shadow-md md:w-90" >
       <h1 className="text-xl font-bold mb-2 text-gray-700 dark:text-gray-200" >Personal details</h1>
@@ -76,7 +89,7 @@ const Intro = () => {
         <DialogTrigger>
           <button className="md:w-80 bg-[#e1e4e8] hover:bg-[#c8cdd2] dark:bg-[#3a3c3d] text-gray-800 dark:text-gray-200 cursor-pointer py-1 rounded-md font-medium" >Edit details</button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-106.25 md:max-w-125 lg:max-w-135 " >
+        <DialogContent className="sm:max-w-106.25 md:max-w-115 lg:max-w-130 " >
           <DialogHeader>
             <DialogTitle className="text-center md:text-xl font-semibold " >Edit details</DialogTitle>
           </DialogHeader>
@@ -91,6 +104,7 @@ const Intro = () => {
                 name="bioText"
                 value={introData.bioText}
                 className="py-1 px-2 md:text-base outline-none border-[1.25px] border-gray-400 rounded-md resize-none"
+                onChange={editDetailsHandler}
               />
             </div>
             <div className="flex flex-col gap-2" >
@@ -102,6 +116,7 @@ const Intro = () => {
                 name="workplace"
                 value={introData.workplace}
                 className="py-1 px-2 md:text-base outline-none border-[1.25px] border-gray-400 rounded-md"
+                onChange={editDetailsHandler}
               />
             </div>
             <div className="flex flex-col gap-2" >
@@ -113,6 +128,7 @@ const Intro = () => {
                 name="Education"
                 value={introData.education}
                 className="py-1 px-2 md:text-base outline-none border-[1.25px] border-gray-400 rounded-md"
+                onChange={editDetailsHandler}
               />
             </div>
 
@@ -125,6 +141,7 @@ const Intro = () => {
                   name="liveIn"
                   value={introData.liveIn}
                   className="py-1 px-2 md:text-base outline-none border-[1.25px] border-gray-400 rounded-md"
+                  onChange={editDetailsHandler}
                 />
               </div>
               <div className="flex flex-col gap-2" >
@@ -135,6 +152,7 @@ const Intro = () => {
                   name="hometown"
                   value={introData.hometown}
                   className="py-1 px-2 md:text-base outline-none border-[1.25px] border-gray-400 rounded-md"
+                  onChange={editDetailsHandler}
                 />
               </div>
             </div>
@@ -148,21 +166,34 @@ const Intro = () => {
                   name="phone"
                   value={introData.phone}
                   className="py-1 px-2 md:text-base outline-none border-[1.25px] border-gray-400 rounded-md"
+                  onChange={editDetailsHandler}
                 />
               </div>
               <div className="flex flex-col gap-2" >
                 <label htmlFor="relationship" className="text-lg font-medium">Relationship</label>
-                <input
-                  type="text"
-                  id="relationship"
+                <select
                   name="relationship"
-                  value={introData.relationship}
+                  id="relationship"
                   className="py-1 px-2 md:text-base outline-none border-[1.25px] border-gray-400 rounded-md"
-                />
+                  onValueChange={editRelationshipHandler}
+                >
+                  <option value="" selected >Choose status</option>
+                  <option value="Single">Single</option>
+                  <option value="In a relationship">In a relationship</option>
+                  <option value="Engaged">Engaged</option>
+                  <option value="Married">Married</option>
+                  <option value="Separated">Separated</option>
+                  <option value="Divorced">Divorced</option>
+                  <option value="Widowed">Widowed</option>
+                </select>
               </div>
             </div>
 
           </div>
+          <DialogFooter>
+            <button className="text-base border border-gray-700 rounded-md cursor-pointer py-1 px-2 hover:bg-gray-100" >Cancel</button>
+            <button className="text-base bg-[#0866ff] hover:bg-[#1755db] text-white rounded-md cursor-pointer py-1 px-2" >Save</button>
+          </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
